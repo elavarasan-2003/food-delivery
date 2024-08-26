1d32522bba71eda93884392016f58a7b3e28c464
Elavarasan T <148007534+elavarasan-2003@users.noreply.github.com>
2024-08-26 22:12:23 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://freakdude2003:UuQy8VbaJUfhFK5z@cluster0.zsye7.mongodb.net/food-del').then(()=>console.log("DataBase Connected"));
+    await mongoose.connect('mongodb+srv://freakdude2003:Zu1BK6NqrSMcAnom@cluster0.iszv6.mongodb.net/food-del').then(()=>console.log("DataBase Connected"));
 }
